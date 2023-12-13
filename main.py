b0185a618191d8aa00e5aec2c02d67b72c9a5906
Shane Karner <shane.karner@gmail.com>
2023-12-13 02:28:44 -0500

    GUI pseudo code

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-# Init
+from tkinter import *
+import math
+
+# UI
+
+window = Tk()
+window.title("Productive Timer")
+window.minsize(width=420, height=420)
+window.config(pady=25, padx=25)
+
+# Label: (title) Productivity Timer (use your time efficiently)
+
+# Timer: the 00:00 looking part
+
+# Label: Total Time spent (How many minutes do you have to work)
+# Spinbox
+
+# Label: How much (%) of that is resting?
+# Scale
+# Label: {Convert % to mins to show user}
+
+# Input: How many breaks would you like?
+# Spinbox
+
+# Label: Calculate (for total time avail, rest time, and break amount)
+# show: [2] 5min breaks after each 25min work period [60mins total])
+
+# Start button
+# Reset button
+
+
+
+window.mainloop()
